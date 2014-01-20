5a99f68ab9182e0cc87135a33b1ec83cb3f9f788
Leonardo Robol <leo@robol.it>
2014-01-20 19:18:17 +0100

    Refactor the inclusion of mpsolve to conform to upstream changes.

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,32 +1,22 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_ARCH),x86)
-MPSOLVE_ARCH := x86-4.8
-else 
-ifeq ($(TARGET_ARCH),mips)
-MPSOLVE_ARCH := mipsel-linux-android-4.8
-else
-MPSOLVE_ARCH := arm-linux-androideabi-4.8
-endif
-endif
-
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES = ../../mpsolve/android-ext-$(MPSOLVE_ARCH)/lib/libmps.a
+LOCAL_SRC_FILES = ../../mpsolve/android-ext-$(TARGET_ARCH_ABI)/lib/libmps.a
 LOCAL_MODULE := libmps
 
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES = ../../mpsolve/android-ext-$(MPSOLVE_ARCH)/lib/libgmp.a
+LOCAL_SRC_FILES = ../../mpsolve/android-ext-$(TARGET_ARCH_ABI)/lib/libgmp.a
 LOCAL_MODULE := libgmp
 
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES = ../mpsolve/android-ext-$(MPSOLVE_ARCH)/include
+LOCAL_C_INCLUDES = ../mpsolve/android-ext-$(TARGET_ARCH_ABI)/include
 LOCAL_MODULE    := libmpsolvebridge
 LOCAL_SRC_FILES := mpsolvebridge.cpp
 LOCAL_LDLIBS += -llog
